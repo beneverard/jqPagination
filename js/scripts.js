e6cfccd2388d2224b6b39d8c94e7f1db26986263
Ben Everard <hello@beneverard.co.uk>
2011-06-20 21:14:42 +0100

    Added link_string to options

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
 	$('.pagination').uzPagination({
 		max_page	:	40,
+		link_string	:	'/?page={page_number}',
 		paged		:	function(page) {
 			$('.log').prepend('<li>Requested page '+page+'</li>');
 		}
