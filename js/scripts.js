7817a872defe69b7ef8ac704fdb57a89c4f24d03
ikkez <ikkez0n3@gmail.com>
2011-12-05 16:24:28 +0100

    added URL hash feature
    when changing the page, a hashTag will append the URL in your browsers addressbar.
    It gives the feature to link to a specific page e.g. index.html#page4

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
 	$('.pagination').jqPagination({
 		link_string	: '/?page={page_number}',
+		url_hash	: '#page{page_number}',		
 		max_page	: 40,
 		paged		: function(page) {
 			$('.log').prepend('<li>Requested page ' + page + '</li>');
